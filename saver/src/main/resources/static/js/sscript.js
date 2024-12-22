5af05f7076e23ff4b6ace5283e4043fd930c7de3
Dhanrajshaky <shakyadhanraj9@gmail.com>
2024-12-22 14:19:36 +0530

    add theme button functionality

--- saver/src/main/resources/static/js/sscript.js
+++ saver/src/main/resources/static/js/sscript.js
@@ -1,1 +1,54 @@
-console.log("Hello from script.js");
+// Ensure currentTheme is defined and applied
+console.log("javascript page loaded")
+let currentTheme = getTheme(); // Get the initial theme from local storage
+document.querySelector("html").classList.add(currentTheme); // Apply the theme to the page
+
+// Change theme function
+function changeTheme() {
+    // Set to button
+    const changeThemeButton = document.querySelector("#theme_change_button");
+    if (changeThemeButton) {
+        changeThemeButton.addEventListener("click", (event) => {
+            const oldTheme = currentTheme;
+            console.log("change theme button clicked");
+            if (currentTheme === "dark") {
+                currentTheme = "light";
+            } else {
+                currentTheme = "dark";
+            }
+            setTheme(currentTheme); // Save the new theme to local storage
+                //remove current theme and add new theme 
+            document.querySelector("html").classList.remove(oldTheme);
+            // add new theme
+            document.querySelector("html").classList.add(currentTheme);
+
+            // Change the button text
+            if (currentTheme === "dark") {
+                changeThemeButton.textContent = "Light";
+            } else {
+                changeThemeButton.textContent = "Dark";
+            }
+
+           
+        });
+    } else {
+        console.error("Button with ID 'theme_change_button' not found");
+    }
+}
+
+// Set theme to local storage
+function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+// Get theme from local storage
+function getTheme() {
+    let theme = localStorage.getItem("theme");
+    if (theme == null) {
+        theme = "light"; // Default theme if no theme is found
+    }
+    return theme;
+}
+
+// Call the function to set up the event listener
+changeTheme();
